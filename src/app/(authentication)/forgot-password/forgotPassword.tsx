1bcdd1bae5cf522f169d1b5884a67fe76422aebd
Zoya Clawson <clawsonzoya@gmail.com>
2025-11-10 19:31:18 -0500

    added login, forgot password pages, added tailwind css globals for styling these components

---
+++ src/app/(authentication)/forgot-password/forgotPassword.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React, { ReactNode, useState } from 'react';
+export default function AuthenticationLayout({
+  children,
+}: {children: ReactNode}){
+  <html lang='en'>
+      {children}
+  </html>
+}
+
