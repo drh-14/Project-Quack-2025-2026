8641ef6c777615e15cccae52e08c9f47c7f8f33e
drh-14 <darrenh1134@gmail.com>
2026-03-02 20:44:19 -0500

    fixed styling

--- frontend/src/app/components/navbar.tsx
+++ frontend/src/app/components/navbar.tsx
@@ -1,4 +1,4 @@
-import Link from "../../../frontend/node_modules/next/link";
+import Link from "next/link";
 import FlightIcon from "@mui/icons-material/Flight";
 import LoginIcon from '@mui/icons-material/Login';
 
